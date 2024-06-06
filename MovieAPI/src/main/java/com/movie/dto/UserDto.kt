ee7645e8ef0881858ae3cd69a2b514c1fb4c643c
Du1727 <dawogdp@gmail.com>
2024-06-06 20:16:57 +0900

    Resolve merge conflicts and update files

--- MovieAPI/src/main/java/com/movie/dto/UserDto.kt
+++ MovieAPI/src/main/java/com/movie/dto/UserDto.kt
@@ -4,31 +4,25 @@ import com.movie.model.Movie
 import com.movie.model.User
 
 data class UserDto @JvmOverloads constructor(
-        val id:Long?,
-        val name: String?,
-        val email:String,
-        val password: String,
-        val movies: Set<Movie>? = HashSet(),
-        val isAdmin: Boolean,
-<<<<<<< HEAD
-=======
-
->>>>>>> origin/IK4P1N4
-){
-    companion object{
+    val id: Long?,
+    val name: String?,
+    val email: String,
+    val password: String,
+    val movies: Set<Movie>? = HashSet(),
+    val isAdmin: Boolean,
+) {
+    companion object {
         @JvmStatic
-        fun convert(from: User):  UserDto{
-            return UserDto(from.id,
-                    from.name,
-                    from.email,
-                    from.password,
-                    from.movieList,
-<<<<<<< HEAD
-                    from.authorities.stream().anyMatch{a-> a.authority.equals("ROLE_ADMIN")})
-=======
-                    from.authorities.stream
-                    ().anyMatch{a-> a.authority.equals("ROLE_ADMIN")})
->>>>>>> origin/IK4P1N4
+        fun convert(from: User): UserDto {
+            return UserDto(
+                from.id,
+                from.name,
+                from.email,
+                from.password,
+                from.movieList,
+                from.authorities.stream()
+                    .anyMatch { a -> a.authority.equals("ROLE_ADMIN") }
+            )
         }
     }
 }
